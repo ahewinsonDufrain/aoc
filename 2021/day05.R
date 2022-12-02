3438960d80d9b5cad94ff2dd5a52a2e8d121e949
Alun Hewinson <ahewinson@dufrain.co.uk>
2022-12-02 22:00:39 +0000

    solve day 5, 2021

--- 2021/day05.R
+++ 2021/day05.R
@@ -18,17 +18,35 @@ parse_hydros <- function(x) {
 is_diagonal <- function(x) {
   (x[1] != x[3]) & (x[2] != x[4])
 }
+draw_line <- function(m, coords) {
+  runs <- case_when(
+    (coords[1] != coords[3]) & (coords[2] != coords[4]) ~ list(xseq = coords[1]:coords[3], yseq = coords[2]:coords[4]),
+    (coords[1] != coords[3])                            ~ list(xseq = coords[1]:coords[3], yseq = rep(coords[2], length(coords[1]:coords[3]))),
+    (coords[2] != coords[4])                            ~ list(xseq = rep(coords[1], length(coords[2]:coords[4])), yseq = coords[2]:coords[4])
+  )
+  for (q in seq_along(runs[[1]])) {
+    m[runs[[2]][q], runs[[1]][q]] <- m[runs[[2]][q], runs[[1]][q]] + 1
+  }
+  m
+}
 
 # part a ------------------------------------------------------------------
 ## solve function for part a
-solve5a <- function(inp) {
+solve5a <- function(inp, discard_diagonals = TRUE) {
   hydros <- inp %>% parse_hydros()
-  hydros <- hydros[!sapply(hydros, is_diagonal)]
+  if (discard_diagonals) hydros <- hydros[!sapply(hydros, is_diagonal)]
+  minx <- sapply(hydros, function(q) q[c(1, 3)]) %>% min()
+  miny <- sapply(hydros, function(q) q[c(2, 4)]) %>% min()
+  maxx <- sapply(hydros, function(q) q[c(1, 3)]) %>% max()
+  maxy <- sapply(hydros, function(q) q[c(2, 4)]) %>% max()
   
+  m <- matrix(0, nrow = 1+maxy, ncol = 1+maxx)
+  
+  for (linje in hydros) {
+    m <- draw_line(m, linje+1)
+  }
+  sum(m > 1)
 }
-
-solve5a(test5) %>% print()
-stop(98)
 
 ## test part a
 test_answer_a <- solve5a(test5)
@@ -39,13 +57,11 @@ pp_try(test_answer_a, expected_a, solve5a(input5), day, "a")
 
 # part b ------------------------------------------------------------------
 ## solve function for part b
-solve5b <- function(inp) {
-  98
-}
+solve5b <- function(inp) solve5a(inp, discard_diagonals = FALSE)
 
 ## test part b
 test_answer_b <- solve5b(test5)
-expected_b    <- -1
+expected_b    <- 12
 
 ## solve part b
 pp_try(test_answer_b, expected_b, solve5b(input5), day, "b")
