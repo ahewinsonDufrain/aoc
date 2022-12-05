6c564c08c59c24f308cfde9f9ab712396bdf3c67
Alun Hewinson <ahewinson@dufrain.co.uk>
2022-12-05 08:43:22 +0000

    solve day 4

--- 2022/day04.R
+++ 2022/day04.R
@@ -9,6 +9,20 @@ input4 %<>% standard_parse_text()
 test4 %<>% standard_parse_text()
 
 # day 4 utils -------------------------------------------------------------
+get_lims <- function(x) {
+  x %>% 
+    str_split(",") %>% 
+    lapply(str_split, "-") %>% 
+    unlist() %>% 
+    as.numeric() %>% 
+    matrix(4)
+}
+test_full_overlap <- function(x) {
+  (x[1] >= x[3] & x[2] <= x[4]) | (x[3] >= x[1] & x[4] <= x[2])
+}
+test_partial_overlap <- function(x) {
+  x[1] <= x[4] & x[2] >= x[3]
+}
 find_common <- function(m) {
   intersect(m[, 1], m[, 2])
 }
@@ -16,12 +30,13 @@ find_common <- function(m) {
 # part a ------------------------------------------------------------------
 ## solve function for part a
 solve4a <- function(inp) {
-  98
+  m <- get_lims(inp)
+  apply(m, 2, test_full_overlap) %>% sum()
 }
 
 ## test part a
 test_answer_a <- solve4a(test4)
-expected_a    <- -1
+expected_a    <- 2
 
 ## solve part a
 pp_try(test_answer_a, expected_a, solve4a(input4), day, "a")
@@ -29,12 +44,13 @@ pp_try(test_answer_a, expected_a, solve4a(input4), day, "a")
 # part b ------------------------------------------------------------------
 ## solve function for part b
 solve4b <- function(inp) {
-  98
+  m <- get_lims(inp)
+  apply(m, 2, test_partial_overlap) %>% sum()
 }
 
 ## test part b
 test_answer_b <- solve4b(test4)
-expected_b    <- -1
+expected_b    <- 4
 
 ## solve part b
 pp_try(test_answer_b, expected_b, solve4b(input4), day, "b")
